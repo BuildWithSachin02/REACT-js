f7aef757dc3af02fa231a5e69e2067beb748a90b
yadavsachin3166@gmail.com <yadavsachin3166@gmail.com>
2026-07-01 21:32:47 +0530

    redux-begins

---
+++ redux-foundations/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <>
+      <div className='container text-center'>
+        <h1>React Redux ToolKit</h1>
+      </div>
+    </>
+  )
+}
---
+++ redux-foundations/src/App/store.js
@@ -0,0 +1,3 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({})//always he will be add in argument with pass objects.
