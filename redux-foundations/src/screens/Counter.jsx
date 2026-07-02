18f5ccb50e862a62d2e7fd5c8223c3fce0746a2b
yadavsachin3166@gmail.com <yadavsachin3166@gmail.com>
2026-07-02 20:28:04 +0530

    updated react-redux projects

--- redux-foundations/src/screens/Counter.jsx
+++ redux-foundations/src/screens/Counter.jsx
@@ -8,6 +8,11 @@ export default function Counter() {
     <>
       <div className="container">
         <h1>React Redux-Toolkit Foundations</h1>
+        <h1>Add Buttons Works only increament</h1>
+        <h1>Decrement buttons works --</h1>
+        <h1>remove buttons do 0 value</h1>
+        <h1>this is only works on 0 to 20 </h1>
+        <h1>let Start</h1>
         <h1>{count}</h1>
         <div className="d-flex gap-3 justify-content-center">
           <button onClick={() => dispatch(increament())}>++</button>
